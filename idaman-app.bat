7f087faa7b88f6a5d428a59901a8037c56bfc917
Raka Suryaardi Widjaja <raka.suryadi@gmail.com>
2021-02-13 10:41:59 +0700

    [ADD]

--- idaman-app.bat
+++ idaman-app.bat
@@ -9,12 +9,14 @@ ECHO "|                [...]                 |"
 ECHO "----------------------------------------"
 ECHO ----------------------------------------
 ECHO A. DATABASE MONGODB
+ECHO B. DATABASE PROMETHEUS
+ECHO C. VISUALIZATION GRAFANA
+ECHO D. MESSAGING RABBITMQ
 ECHO ----------------------------------------
-ECHO C1. MONGODB + WEBAPP IDAMAN.ID + GATEWAY WEB, DOCUMENT + 
-         SERVICE INSTITUTION, PREFERENCE, USER + MERCHANT + PAYMENT
+ECHO C1. MONGODB + WEBAPP MAIN + GATEWAY WEB, DOCUMENT + SERVICE INSTITUTION, PREFERENCE, USER, MERCHANT, PAYMENT
 ECHO C2. MONGODB + GATEWAY ANDROID + SERVICE INSTITUTION, PREFERENCE, USER
 ECHO ----------------------------------------
-ECHO 1. WEBAPP IDAMAN.ID
+ECHO 1. WEBAPP MAIN
 
 ECHO 21. GATEWAY WEB
 ECHO 22. GATEWAY ANDROID
@@ -27,6 +29,7 @@ ECHO 33. SERVICE PAYMENT
 ECHO 34. SERVICE PREFERENCE
 ECHO 35. SERVICE SEARCH
 ECHO 36. SERVICE USER
+ECHO 37. SERVICE NOTIFICATION SYSTEM
 ECHO ----------------------------------------
 
 SET /p menus="CHOOSE PROGRAMS TO RUN (comma separated): "
@@ -42,7 +45,7 @@ FOR %%i in ("%menus:,=" "%") do (
     START "Service Preference" CMD /k "cd /d D:\startup\idaman\service\preference\preference && npm run dev"
     START "Service User" CMD /k "cd /d D:\startup\idaman\service\user\user && npm run dev"
     START "Service Merchant" CMD /k "cd /d D:\startup\idaman\service\merchant\merchant && npm run dev"
-    
+
     START "Service Payment" CMD /k "cd /d D:\startup\idaman\service\payment\payment && npm run dev"
     START "Service Payment Test" CMD /k "cd /d D:\startup\idaman\service\payment\payment && npm run test-minimal"
   )
@@ -56,6 +59,16 @@ FOR %%i in ("%menus:,=" "%") do (
 
   IF %%i=="A" (
     START "Database MongoDB" CMD /k "cd /d D:\MongoDB\Server\4.4\bin && mongod --port 27017 --dbpath D:\MongoDB\Server\4.4\data --replSet rs0 --bind_ip localhost"
+  )
+  IF %%i=="B" (
+    START "Database Prometheus" CMD /k "cd /d D:\prometheus\prometheus-2.21.0 && prometheus.exe"
+  )
+  IF %%i=="C" (
+    START "Visualization Grafana" CMD /k "cd /d D:\grafana\grafana-7.3.7\bin && grafana-server.exe"
+  )
+  IF %%i=="D" (
+    START "Messaging RabbitMQ" CMD /k "cd /d C:\Program Files\RabbitMQ Server\rabbitmq_server-3.8.11\sbin && rabbitmq-server start"
+    START "Messaging RabbitMQ Dashboard" CMD /k "cd /d C:\Program Files\RabbitMQ Server\rabbitmq_server-3.8.11\sbin && rabbitmq-plugins.bat enable rabbitmq_management"
   )
   IF %%i=="1" (
     START "Webapp Idaman.ID" CMD /k "cd /d D:\startup\idaman\app\idaman-id && npm run dev"
@@ -92,6 +105,9 @@ FOR %%i in ("%menus:,=" "%") do (
   )
   IF %%i=="36" (
     START "Service User" CMD /k "cd /d D:\startup\idaman\service\user\user && npm run dev"
+  )
+  IF %%i=="37" (
+    START "Service Notification System" CMD /k "cd /d D:\startup\idaman\service\notification\system && npm run dev"
   )
 
   ECHO Success running: %%i
