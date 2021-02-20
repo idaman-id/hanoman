8c68fac256adb5d80497ea8ca9a8d4442c89adb5
Raka Suryaardi Widjaja <raka.suryadi@gmail.com>
2021-02-20 12:53:37 +0700

    [CHG] update automation

--- idaman-app.bat
+++ idaman-app.bat
@@ -13,7 +13,7 @@ ECHO B. DATABASE PROMETHEUS
 ECHO C. VISUALIZATION GRAFANA
 ECHO D. MESSAGING RABBITMQ
 ECHO ----------------------------------------
-ECHO C1. MONGODB + WEBAPP MAIN + GATEWAY WEB, DOCUMENT + SERVICE INSTITUTION, PREFERENCE, USER, MERCHANT, PAYMENT
+ECHO C1. MONGODB + WEBAPP MAIN + GATEWAY WEB, DOCUMENT + SERVICE INSTITUTION, PREFERENCE, USER, PAYMENT
 ECHO C2. MONGODB + GATEWAY ANDROID + SERVICE INSTITUTION, PREFERENCE, USER
 ECHO ----------------------------------------
 ECHO 1. WEBAPP MAIN
@@ -21,15 +21,23 @@ ECHO 1. WEBAPP MAIN
 ECHO 21. GATEWAY WEB
 ECHO 22. GATEWAY ANDROID
 ECHO 23. GATEWAY PAYMENT
-ECHO 24. GATEWAY DOCUMENT
+ECHO 24. GATEWAY DOCUMENT (LINK)
+ECHO 25. GATEWAY NOTIFICATION (LINK)
+ECHO 26. GATEWAY STORAGE (LINK)
+ECHO 27. GATEWAY STORAGE SERVANT (LINK)
 
 ECHO 31. SERVICE INSTITUTION
-ECHO 32. SERVICE MERCHANT
-ECHO 33. SERVICE PAYMENT
-ECHO 34. SERVICE PREFERENCE
-ECHO 35. SERVICE SEARCH
-ECHO 36. SERVICE USER
-ECHO 37. SERVICE NOTIFICATION SYSTEM
+ECHO 32. SERVICE PAYMENT
+ECHO 33. SERVICE PREFERENCE
+ECHO 34. SERVICE REVIEW
+ECHO 35. SERVICE SYSTEM NOTIFICATION
+ECHO 36. SERVICE SYSTEM STORAGE (LINK)
+ECHO 37. SERVICE USER
+
+ECHO XX. SERVICE MEETING (DISABLED)
+ECHO XX. SERVICE LOG (DISABLED)
+ECHO XX. SERVICE SEARCH (DISABLED)
+ECHO XX. SERVICE MERCHANT (DISABLED)
 ECHO ----------------------------------------
 
 SET /p menus="CHOOSE PROGRAMS TO RUN (comma separated): "
@@ -39,22 +47,20 @@ FOR %%i in ("%menus:,=" "%") do (
   IF %%i=="C1" (
     START "Database MongoDB" CMD /k "cd /d D:\MongoDB\Server\4.4\bin && mongod --port 27017 --dbpath D:\MongoDB\Server\4.4\data --replSet rs0 --bind_ip localhost"
     START "Webapp Idaman.ID" CMD /k "cd /d D:\startup\idaman\app\idaman-id && npm run dev"
-    START "Gateway Web" CMD /k "cd /d D:\startup\idaman\service\endpoint\web && npm run dev"
-    START "Gateway Document" CMD /k "cd /d D:\startup\idaman\service\endpoint\document && npm run dev"
-    START "Service Institution" CMD /k "cd /d D:\startup\idaman\service\institution\institution && npm run dev"
-    START "Service Preference" CMD /k "cd /d D:\startup\idaman\service\preference\preference && npm run dev"
-    START "Service User" CMD /k "cd /d D:\startup\idaman\service\user\user && npm run dev"
-    START "Service Merchant" CMD /k "cd /d D:\startup\idaman\service\merchant\merchant && npm run dev"
 
-    START "Service Payment" CMD /k "cd /d D:\startup\idaman\service\payment\payment && npm run dev"
-    START "Service Payment Test" CMD /k "cd /d D:\startup\idaman\service\payment\payment && npm run test-minimal"
+    START "Gateway Web" CMD /k "cd /d D:\startup\idaman\gateway\gateway-web && npm run dev"
+    START "Service Institution" CMD /k "cd /d D:\startup\idaman\service\service-institution && npm run dev"
+    START "Service Preference" CMD /k "cd /d D:\startup\idaman\service\service-preference && npm run dev"
+    START "Service User" CMD /k "cd /d D:\startup\idaman\service\service-user && npm run dev"
+    START "Service Payment" CMD /k "cd /d D:\startup\idaman\service\service-payment && npm run dev"
   )
   IF %%i=="C2" (
     START "Database MongoDB" CMD /k "cd /d D:\MongoDB\Server\4.4\bin && mongod --port 27017 --dbpath D:\MongoDB\Server\4.4\data --replSet rs0 --bind_ip localhost"
-    START "Gateway Android" CMD /k "cd /d D:\startup\idaman\service\endpoint\android && npm run dev"
-    START "Service Institution" CMD /k "cd /d D:\startup\idaman\service\institution\institution && npm run dev"
-    START "Service Preference" CMD /k "cd /d D:\startup\idaman\service\preference\preference && npm run dev"
-    START "Service User" CMD /k "cd /d D:\startup\idaman\service\user\user && npm run dev"
+
+    START "Gateway Android" CMD /k "cd /d D:\startup\idaman\gateway\gateway-android && npm run dev"
+    START "Service Institution" CMD /k "cd /d D:\startup\idaman\service\service-institution && npm run dev"
+    START "Service Preference" CMD /k "cd /d D:\startup\idaman\service\service-preference && npm run dev"
+    START "Service User" CMD /k "cd /d D:\startup\idaman\service\service-user && npm run dev"
   )
 
   IF %%i=="A" (
@@ -70,45 +76,70 @@ FOR %%i in ("%menus:,=" "%") do (
     START "Messaging RabbitMQ" CMD /k "cd /d C:\Program Files\RabbitMQ Server\rabbitmq_server-3.8.11\sbin && rabbitmq-server start"
     START "Messaging RabbitMQ Dashboard" CMD /k "cd /d C:\Program Files\RabbitMQ Server\rabbitmq_server-3.8.11\sbin && rabbitmq-plugins.bat enable rabbitmq_management"
   )
+
+  
   IF %%i=="1" (
     START "Webapp Idaman.ID" CMD /k "cd /d D:\startup\idaman\app\idaman-id && npm run dev"
   )
 
   IF %%i=="21" (
-    START "Gateway Web" CMD /k "cd /d D:\startup\idaman\service\endpoint\web && npm run dev"
+    START "Gateway Web" CMD /k "cd /d D:\startup\idaman\gateway\gateway-web && npm run dev"
   )
   IF %%i=="22" (
-    START "Gateway Android" CMD /k "cd /d D:\startup\idaman\service\endpoint\android && npm run dev"
+    START "Gateway Android" CMD /k "cd /d D:\startup\idaman\gateway\gateway-android && npm run dev"
   )
   IF %%i=="23" (
-    START "Gateway Payment" CMD /k "cd /d D:\startup\idaman\service\endpoint\payment && npm run dev"
+    START "Gateway Payment" CMD /k "cd /d D:\startup\idaman\gateway\gateway-payment && npm run dev"
   )
   IF %%i=="24" (
-    START "Gateway Document" CMD /k "cd /d D:\startup\idaman\service\endpoint\document && npm run dev"
+    START "Gateway Document" CMD /k "cd /d D:\startup\idaman\gateway\gateway-document"
+    START "Gateway Document Test" CMD /k "cd /d D:\startup\idaman\gateway\gateway-document & composer test"
+  )
+  IF %%i=="25" (
+    START "Gateway Notification" CMD /k "cd /d D:\startup\idaman\gateway\gateway-notification"
+    START "Gateway Notification Test" CMD /k "cd /d D:\startup\idaman\gateway\gateway-notification & composer test"
+  )
+  IF %%i=="26" (
+    START "Gateway Storage" CMD /k "cd /d D:\startup\idaman\gateway\gateway-storage"
+    START "Gateway Storage Test" CMD /k "cd /d D:\startup\idaman\gateway\gateway-storage & composer test"
+  )
+  IF %%i=="27" (
+    START "Gateway Storage Servant" CMD /k "cd /d D:\startup\idaman\gateway\gateway-storage-servant"
+    START "Gateway Storage Servant Test" CMD /k "cd /d D:\startup\idaman\gateway\gateway-storage-servant & composer test"
   )
 
+
+
   IF %%i=="31" (
-    START "Service Institution" CMD /k "cd /d D:\startup\idaman\service\institution\institution && npm run dev"
+    START "Service Institution" CMD /k "cd /d D:\startup\idaman\service\service-institution && npm run dev"
+    START "Service Institution Test" CMD /k "cd /d D:\startup\idaman\service\service-institution && npm run test-minimal"
   )
   IF %%i=="32" (
-    START "Service Merchant" CMD /k "cd /d D:\startup\idaman\service\merchant\merchant && npm run dev"
+    START "Service Payment" CMD /k "cd /d D:\startup\idaman\service\service-payment && npm run dev"
+    START "Service Payment Test" CMD /k "cd /d D:\startup\idaman\service\service-payment && npm run test-minimal"
   )
   IF %%i=="33" (
-    START "Service Payment" CMD /k "cd /d D:\startup\idaman\service\payment\payment && npm run dev"
-    START "Service Payment Test" CMD /k "cd /d D:\startup\idaman\service\payment\payment && npm run test-minimal"
+    START "Service Preference" CMD /k "cd /d D:\startup\idaman\service\service-preference && npm run dev"
+    START "Service Preference Test" CMD /k "cd /d D:\startup\idaman\service\service-preference && npm run test-minimal"
   )
   IF %%i=="34" (
-    START "Service Preference" CMD /k "cd /d D:\startup\idaman\service\preference\preference && npm run dev"
+    START "Service Review" CMD /k "cd /d D:\startup\idaman\service\service-review && npm run dev"
+    START "Service Review Test" CMD /k "cd /d D:\startup\idaman\service\service-review && npm run test-minimal"
   )
   IF %%i=="35" (
-    START "Service Search" CMD /k "cd /d D:\startup\idaman\service\preference\search && npm run dev"
+    START "Service System Notification" CMD /k "cd /d D:\startup\idaman\service\service-system-notification && npm run dev"
+    START "Service System Notification Test" CMD /k "cd /d D:\startup\idaman\service\service-system-notification && npm run test-minimal"
   )
   IF %%i=="36" (
-    START "Service User" CMD /k "cd /d D:\startup\idaman\service\user\user && npm run dev"
+    START "Service System Storage" CMD /k "cd /d D:\startup\idaman\service\service-system-storage"
+    START "Service System Storage Test" CMD /k "cd /d D:\startup\idaman\service\service-system-storage & composer test"
   )
   IF %%i=="37" (
-    START "Service Notification System" CMD /k "cd /d D:\startup\idaman\service\notification\system && npm run dev"
+    START "Service User" CMD /k "cd /d D:\startup\idaman\service\service-user && npm run dev"
+    START "Service User Test" CMD /k "cd /d D:\startup\idaman\service\service-user && npm run test-minimal"
   )
+
+
 
   ECHO Success running: %%i
   ECHO ----------------------------------------
